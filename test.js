f3c1c05fa7bed69acc7ca440b623942bec83fc22
grachev <grachev@grachev.grachev>
2023-12-23 19:35:11 +0400

    task

---
+++ test.js
@@ -0,0 +1,36 @@
+// 1.
+// console.log(31, 18, 79);
+
+// 2.
+// console.log(`${47}  ${52}  ${150}`);
+
+// 3.
+// console.log(`${50}\n${10}`);
+
+// 4.
+// console.log(`${5}\n${10}\n${21}`);
+
+// 5.
+// console.log(`${1}\n\n${2}`);
+
+// 6.
+// let a = 3.41241423;
+// let b = a.toFixed(3);
+// console.log(b);
+
+// 7.
+// let a = 2.71828;
+// let b = a.toFixed(1);
+// console.log(b)
+
+// 8. 
+// npm install readline-sync
+// let a = require('readline-sync');
+// let b = a.question('Введите число: ');
+// console.log('Вы ввели: ', b)
+
+// 9.
+// let a = require('readline-sync');
+// let b = a.question('Введите число: ');
+// console.log(b, ' - вот такое число Вы ввели')
+
