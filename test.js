2dd614f719914b63f02f6f3da7a921bfa2bbdaff
grachev <grachev@grachev.grachev>
2023-12-25 11:51:33 +0400

    17

--- test.js
+++ test.js
@@ -34,3 +34,58 @@
 // let b = a.question('Введите число: ');
 // console.log(b, ' - вот такое число Вы ввели')
 
+// 10.
+// let a = require('readline-sync');
+// let b = a.question('Введите ваше имя: ')
+// console.log(b, ' - Ваше имя')
+
+// 11.
+// let a = require('readline-sync');
+// let b = a.question('Введите название футбольной команды: ')
+// console.log(b, '<-- Лучшая футбольная команда')
+
+// 12.
+// let a = require('readline-sync');
+// let b = a.question('Введите ваше имя: ');
+// console.log('Привет, ', b, '!')
+
+// 13.
+// let a = require('readline-sync');
+// let b = Number(a.question('Введите число: '));
+// console.log('Следующее за числом', b, 'число - ', b+1, '.');
+// console.log('Для числа ', b, 'предыдущее число - ', b-1, '.')
+
+// 14.
+// let init = require('readline-sync');
+// let a = Number(init.question('Введите число: '));
+// let b = Number(init.question('Введите число: '));
+// let c = Number(init.question('Введите число: '));
+// console.log(a, '  ', b, '  ', c)
+
+// 15.
+// let init = require('readline-sync');
+// let a = Number(init.question('Введите число: '));
+// let b = Number(init.question('Введите число: '));
+// let c = Number(init.question('Введите число: '));
+// let d = Number(init.question('Введите число: '));
+// let s = ' ';
+// console.log(a, s, b, s, c, s, d)
+
+// 16.
+// let init = require('readline-sync');
+// let t = Number(init.question('add number: '));
+// let v = Number(init.question('add number: '));
+// let x = Number(init.question('add number: '));
+// let y = Number(init.question('add number: '));
+// console.log(`а) 5 10 \t б) 100 ${t} \t в) ${x} 25`);
+// console.log(`   7 см \t 1949 ${v} \t ${x} ${y}`);
+
+// 17.
+let init = require('readline-sync');
+let a = Number(init.question('add number: '));
+let b = Number(init.question('add number: '));
+let x = Number(init.question('add number: '));
+let y = Number(init.question('add number: '));
+console.log(`a) 2 kg \t b) ${a} 1 \t v) ${x} ${y}`);
+console.log(`   13 17 \t 19 ${b} \t\t 5 ${y}`);
+
--- test.py
+++ test.py
@@ -26,5 +26,54 @@
 # print('Вы ввели: ', a)
 
 # 9.
-a = input('Введите число: ')
-print(a, ' - вот такое число Вы ввели')
+# a = input('Введите число: ')
+# print(a, ' - вот такое число Вы ввели')
+
+# 10.
+# a = input('Введите свое имя: ')
+# print('Ваше имя ', a)
+
+# 11.
+# a = input('Введите название футбольной команды: ')
+# print(a, '<-- лучшая футбольная команда!!!')
+
+# 12. 
+# a = input('Введите ваше имя: ')
+# print('Привет, ', a, '!')
+
+# 13.
+# a = int(input('Введите число: '))
+# print('Следующее за числом', a, 'число - ', a+1, '.')
+# print('Для числа ', a, 'предыдущее число - ', a-1, '.')
+
+# 14.
+# a = input('Введите число :')
+# b = input('Введите число :')
+# c = input('Введите число :')
+# print(a, '  ', b, '  ', c)
+
+# 15.
+# a = input('Введите число: ')
+# b = input('Введите число: ')
+# c = input('Введите число: ')
+# d = input('Введите число: ')
+# s = ' '
+# print(a, s, b, s, c, s, d)
+
+# 16.
+# t = input('add number: ')
+# v = input('add number: ')
+# x = input('add number: ')
+# y = input('add number: ')
+# print(f'a) 5 10 \t б) 100 {t} \t в) {x} 25')
+# print(f'   7 см \t 1949 {v} \t {x} {y}')
+
+# 17.
+# a = input('add number: ')
+# b = input('add number: ')
+# x = input('add number: ')
+# y = input('add number: ')
+# print(f'a) 2 kg \t b) {a} 1 \t b) {x} {y}')
+# print(f'   13 17 \t 19 {b} \t\t 5 {y}')
+
+
